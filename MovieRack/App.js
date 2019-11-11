db6257e508e4172519c2d50e080113b2abc1b13b
Maheshss1 <42149692+Maheshss1@users.noreply.github.com>
2019-11-12 00:35:27 +0530

    Added

---
+++ MovieRack/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AppNavigator from './support/Navigator';
+
+export default class App extends React.Component{
+  render(){
+    return(
+        <AppNavigator />
+    )
+  }
+}
